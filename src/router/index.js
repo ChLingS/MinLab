db681735bfc1a965b5476e036cf7690db975e77b
ChLingS <147455543+ChLingS@users.noreply.github.com>
2025-02-09 14:00:51 +0800

    4

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   mode:'hash',
-  history: createWebHashHistory('/warming/'),
+  history: createWebHashHistory('/MinLab/'),
   routes: [
     {
       path: '/',
