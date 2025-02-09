53ffbe6b1ac83bd32f36c0acbca345d543ef34e5
ChLingS <147455543+ChLingS@users.noreply.github.com>
2025-02-09 14:31:17 +0800

    re

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  mode:'hash',
-  history: createWebHashHistory('/MinLab/'),
+  history: createWebHistory('/MinLab/'),
   routes: [
     {
       path: '/',
